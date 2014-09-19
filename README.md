8c2d5852942f8919e3214cac339919ad87f9d022
Vic <452@list.ru>
2014-09-19 16:51:50 +0400

    First iteration. Small functonal.

--- README.md
+++ README.md
@@ -2,3 +2,24 @@ jQueryYandexTranslate
 =====================
 
 jQuery plugin for yandex translate api
+
+Test example code:
+
+
+<script type="text/javascript" src="yaTranslate.js"></script>
+<script type="text/javascript">
+/*<![CDATA[*/
+
+jQuery(function($) {
+	var settings = {
+		srclang: 'ru',                                // source language
+		dstlang: 'en,fr',                             // destination languages, comma separated list
+		apikey: 'api_key_you_can_get_on_yandex_site', // http://api.yandex.ru/key/form.xml?service=trnsl
+	};
+
+	jQuery(".textelements").yaTranslate(settings);
+});
+
+/*]]>*/
+</script>
+
---
+++ yaTranslate.js
@@ -0,0 +1,148 @@
+;
+
+(function($) {
+
+	var setting = {
+	    	srclang: 'ru',
+	    	dstlang: 'en,fr',
+	    	dstlanglist: {},
+
+	    	apikey: '',
+	    	adrlanglist: 'https://translate.yandex.net/api/v1.5/tr.json/getLangs?key=[apikey]&ui=[srclang]',
+	    	adrtranslate: 'https://translate.yandex.net/api/v1.5/tr.json/translate?key=[apikey]&text=[text]&lang=[lang]&format=[format]',
+
+	    	showtranslatedirs: null,
+	    	hidetranslatedirs: null
+	    },
+
+	    oTranslateDiv = null,
+	    sOldParentPos = '',
+	    bLeaveObject = true,
+
+	    showTranslateText = function(adr, obResult) {
+	      $.getJSON(
+	        adr,
+	        function(data) {
+	        	if( ('code' in data) && (data.code == 200) && ('text' in data) ) {
+	        		console.log('data = ', data);
+	        		obResult.html(data.text[0]);
+	        	}
+	        }
+	      );
+	    },
+
+	    showTranslateMenu = function(ob) {
+		  	var dstList = setting.dstlanglist,
+		  	    s = '',
+		  	    sAdr = '',
+		  	    oParent = $(ob);
+
+	      		if( !$.isEmptyObject(dstList) ) {
+	      			if( !oTranslateDiv ) {
+	      				oTranslateDiv = $('<div />')
+	      				                  .css({position: 'absolute', top: '-4px', right: 0, border: '1px solid #000000', color: '#770000', backgroundColor: '#ffff00', display: 'none', padding: '3px'})
+	      				                  .appendTo('body');
+	      			}
+	      			oTranslateDiv.html('');
+	      			for(s in dstList) {
+	      				sAdr = setting.adrtranslate
+	      				  .replace(/\[apikey\]/, setting.apikey)
+	      				  .replace(/\[lang\]/, setting.srclang + '-' + s)
+	      				  .replace(/\[text\]/, encodeURIComponent(oParent.text()))
+	      				  .replace(/\[format\]/, 'plain');
+
+	      				$('<a> ' + s + ' </a>')
+	      				  .attr('href', sAdr)
+	      				  .attr('title', dstList[s])
+	      				  .appendTo(oTranslateDiv);
+	      			}
+	      			sOldParentPos = oParent.css('position');
+	      			oParent.css({position: 'relative'});
+	      			oTranslateDiv
+	      			  .appendTo(oParent)
+	      			  .show()
+	      			  .find('a')
+	      			  .on('click', function(event) { event.preventDefault(); showTranslateText($(this).attr('href'), oTranslateDiv); return false; } );
+	      			console.log('set lang menu'); // , setting
+	      		}
+	    },
+
+	    hideTranslateMenu = function(ob) {
+		  	var oParent = $(ob);
+	    	if( oTranslateDiv ) {
+	    		oTranslateDiv.hide();
+	    	}
+	    	if( sOldParentPos !== '' ) {
+	    		oParent.css({position: sOldParentPos});
+	    		sOldParentPos = '';
+	    	}
+	    };
+
+	$.fn.yaTranslate = function(param) {
+		var adrList = setting.adrlanglist,
+		    dstList = setting.dstlanglist;
+
+		setting = $.extend(setting, param || {});
+
+		adrList = adrList
+			.replace(/\[apikey\]/, setting.apikey)
+			.replace(/\[srclang\]/, setting.srclang);
+
+		if( $.isEmptyObject(dstList) ) {
+			$.getJSON(
+				adrList,
+				function(data) {
+					var dirs = data.dirs || [],
+					    langs = data.langs || {},
+					    aDstLang = setting.dstlang.split(','),
+					    srcPattern = setting.srclang.toLocaleLowerCase() + '-',
+					    nPatternLength = srcPattern.length,
+					    i = 0,
+					    nMax = 0,
+					    s = '';
+					for(i = 0, nMax = aDstLang.length; i < nMax; i = i + 1) {
+						s = aDstLang[i].replace(/^\s+|\s+$/g, '').toLocaleLowerCase();
+						dstList[s] = '';
+					}
+
+					for(i = dirs.length - 1; i >= 0; i = i - 1) {
+						if( dirs[i].indexOf(srcPattern) !== 0 ) {
+							continue;
+						}
+						s = dirs[i].substr(nPatternLength);
+//						console.log(dirs[i] + ' -> ' + s);
+						if( (s in dstList) && (s in langs) ) {
+							dstList[s] = langs[s];
+						}
+					}
+//					console.log('setting = ', setting);
+				}
+			);
+		}
+
+//		console.log('setting = ', setting);
+		return this
+		        .on( "mouseenter", function(event) {
+		        	if( bLeaveObject ) {
+			        	if( setting.showtranslatedirs ) {
+			        		setting.showtranslatedirs(this);
+			        	}
+			        	else {
+			        		showTranslateMenu(this);
+			        	}
+			        }
+		        	bLeaveObject = false;
+		        })
+		        .on( "mouseleave", function(event) {
+		        	bLeaveObject = true;
+		        	if( setting.hidetranslatedirs ) {
+		        		setting.hidetranslatedirs(this);
+		        	}
+		        	else {
+		        		hideTranslateMenu(this);
+		        	}
+		        });
+
+}
+
+})(jQuery)
